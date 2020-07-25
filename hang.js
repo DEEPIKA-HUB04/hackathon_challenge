100ea0aad42be1737884c9b98e11376ff6dec040
DEEPIKA-HUB04 <DEEPIKA-HUB04@users.noreply.github.com>
2020-07-25 23:35:31 +0530

    inital commit

--- hang.js
+++ hang.js
@@ -1,3 +1,4 @@
+
 var words = [
 	"python",
 	"javascript",
@@ -15,24 +16,29 @@ var words = [
 	"ruby"
 ]
 
-let answer = "";
-let maxwrong = 6;
-let wrong = 0;
-let guessed = [];
-let currentWordStatus = null;
+//var difficultWords = [ "Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia"]
+
+var answer = "";
+var maxwrong = 6;
+var wrong = 0;
+var correct = 0;
+var guessed = [];
+var currentWordStatus = null;
 
 // function to generate random words
 function randomWord() {
+
     return answer = words[Math.floor(Math.random() * words.length)];
+    
   }
 
 
   // function to display buttons and 
   function displayButtons() {
-    let button = 'abcdefghijklmnopqrstuvwxyz'.split('').map(alphabet =>
+    var  button = 'abcdefghijklmnopqrstuvwxyz'.split('').map(alphabet =>
       `
         <button
-          style = "background-color:blue; margin : 8px 4px ; width: 30px"
+          style = "background-color:coral; margin : 8px 8px ; width: 40px;font-size : 20px;"
           id='` + alphabet + `'
           onClick="Guess('` + alphabet + `')"
         >
@@ -43,7 +49,51 @@ function randomWord() {
     document.getElementById('keypress').innerHTML = button;
   }
 
+  function Guess(selectedLetter){
+    guessed.indexOf(selectedLetter) === -1 ? guessed.push(selectedLetter) : null;
+    document.getElementById(selectedLetter).setAttribute('disabled', true);
+
+    if (answer.indexOf(selectedLetter) >= 0) {   
+        correct++;
+        document.getElementById("count").innerHTML = correct;                                         // if the guessed letter is correct will display the letter
+      guessWord(); 
+      seeIfGameWon();
+      nextlevel();
+    }
+    
+    else if (answer.indexOf(selectedLetter) === -1) {              // if not correct will increment wrong count by 1.
+        
+        wrong++;
+         updateMistakes();
+        seeIfGameLost();
+        updatePic();
+    }
+
+  }
+  function updatePic() {
+    document.getElementById('hangmanphoto').src = 'images/' + wrong + '.jpg';
+  }
   
+  function seeIfGameWon() {
+    if (currentWordStatus === answer) {   
+      document.getElementById('keypress').innerHTML = 'Score : ' + correct + " " + 'HURRAY YOU WON!!!' ;
+    }  
+   
+  }
+
+  function nextlevel() {
+
+    
+      
+  }
+  
+  function seeIfGameLost() {
+    if (wrong === maxwrong) {
+      document.getElementById('fillwords').innerHTML = 'The answer was: ' + answer;
+      document.getElementById('keypress').innerHTML = 'OOPS YOU LOST!!!';
+    }
+  }
+
 
   // function to provide blank dashes depending on the random answer generated
   function guessWord() {
@@ -56,9 +106,25 @@ function randomWord() {
     document.getElementById('wrong').innerHTML = wrong;
   }
   
+  function reset() {
+    wrong= 0;
+    guessed = [];
+    document.getElementById('hangmanphoto').src = 'images/0.jpg';
   
-
+    randomWord();
+    guessWord();
+    updateMistakes();
+    displayButtons();
+  }
+  
   document.getElementById("maxwrong").innerHTML = maxwrong; 
   randomWord();
  displayButtons();
  guessWord();
+
+ // NEXT LEVEL
+
+ /*function nextlevel (difficultWords){
+    return answer = difficultWords[Math.floor(Math.random() * difficultWords.length)];
+ }*/
+
